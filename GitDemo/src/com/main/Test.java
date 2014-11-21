d10f4747d3d048ba10fbadddb1c07b14ba72116a
machen <86411995@qq.com>
2014-11-21 16:42:41 +0800

    ff

--- GitDemo/src/com/main/Test.java
+++ GitDemo/src/com/main/Test.java
@@ -6,7 +6,7 @@ public class Test {
         System.err.println("2");
         System.out.println("3");
         System.out.println("bug OK");
-        System.err.println("222222222fffff");
+        System.err.println("2222222ff22fffff");
         System.err.println("222222222fffff");
     }
 }
