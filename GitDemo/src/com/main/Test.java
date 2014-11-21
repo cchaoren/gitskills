d2c72e33e012ad7da86bcf4a02c415b3048c97ed
machen <86411995@qq.com>
2014-11-21 15:22:06 +0800

    m commit

--- GitDemo/src/com/main/Test.java
+++ GitDemo/src/com/main/Test.java
@@ -3,5 +3,7 @@ package com.main;
 public class Test {
     public static void main(String[] args) {
         System.out.println("1");
+        System.err.println("2");
+        System.out.println("3");
     }
 }
